cbf80cf0a3ddc80286bbbc4609ec55fbd9e53fc2
lewispricey <1995ldp@gmail.com>
2022-08-01 13:55:29 +0100

    removes commented out console logs

--- controllers/topics.js
+++ controllers/topics.js
@@ -2,13 +2,8 @@ const { fetchTopics } = require("../models/topics")
 
 
 exports.getTopics = (req, res, next) => {
-    // console.log("in controller...")
     fetchTopics()
     .then((topics) => {
-        // console.log(topics)
         res.status(200).send(topics)
     })
-    .catch((err) => {
-        res.status(500).send("server error...")
-    }) 
 }
--- models/topics.js
+++ models/topics.js
@@ -1,7 +1,6 @@
 const db = require("../db/connection")
 
 exports.fetchTopics = () => {
-    // console.log("reached model!")
     return db
     .query("SELECT * FROM topics")
     .then(({rows}) => rows)
