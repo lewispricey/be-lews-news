9387c744ee8e38d7d94cf25aad6aa275a21de316
lewispricey <1995ldp@gmail.com>
2022-08-02 11:34:37 +0100

    adds user endpoint with GET method

---
+++ __tests__/users.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+const app = require("../app.js");
+const seedDB = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const db = require("../db/connection");
+
+beforeAll(() => seedDB(data))
+afterAll(() => db.end())
+
+
+describe("/api/users", () => {
+    describe("GET", () => {
+        test("Status 200 - returns an array of objects with the required keys", async () => {
+            const {body} = await request(app).get('/api/users').expect(200)
+            body.users.forEach((user) => {
+                expect(typeof user).toBe("object")
+                expect(user.hasOwnProperty("username")).toBe(true)
+                expect(user.hasOwnProperty("name")).toBe(true)
+                expect(user.hasOwnProperty("avatar_url")).toBe(true)
+            })
+        })
+        test("Status 200 - returns the correct amount of user objects back", async () => {
+            const {body} = await request(app).get('/api/users').expect(200)
+            expect(body.users.length).toBe(4)
+        })
+    })
+})
---
+++ controllers/users.js
@@ -0,0 +1,5 @@
+const { fetchUsers } = require("../models/users")
+
+exports.getUsers = (req, res, next) => {
+    fetchUsers().then((users) => res.status(200).send({users}))
+}
