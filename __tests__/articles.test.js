b0c6f46ef88e24985c08bf4b1775fbff0a0104b0
lewispricey <1995ldp@gmail.com>
2022-08-02 13:04:25 +0100

    refactor tests to use async/await rarther than promise chains

--- __tests__/articles.test.js
+++ __tests__/articles.test.js
@@ -9,29 +9,21 @@ afterAll(() => db.end())
 
 describe("/api/articles/:article_id", () => {
     describe("GET", () => {
-        test("STATUS:200 returns an object", () => {
-            return request(app)
-            .get('/api/articles/1')
-            .expect(200)
-            .then(({body}) => {
-                expect(typeof body).toBe("object")
-            })
+        test("STATUS:200 returns an object", async () => {
+            const {body} = await request(app).get('/api/articles/1').expect(200)
+            expect(typeof body).toBe("object")
         })
-        test("STATUS:200 return object contains the required keys", () => {
-            return request(app)
-            .get('/api/articles/1')
-            .expect(200)
-            .then(({body}) => {
-                expect(body.hasOwnProperty("author")).toBe(true)
-                expect(body.hasOwnProperty("title")).toBe(true)
-                expect(body.hasOwnProperty("article_id")).toBe(true)
-                expect(body.hasOwnProperty("body")).toBe(true)
-                expect(body.hasOwnProperty("topic")).toBe(true)
-                expect(body.hasOwnProperty("votes")).toBe(true)
-                expect(body.hasOwnProperty("comment_count")).toBe(true)
-            })
+        test("STATUS:200 return object contains the required keys", async () => {
+            const {body} = await request(app).get('/api/articles/1').expect(200)
+            expect(body.hasOwnProperty("author")).toBe(true)
+            expect(body.hasOwnProperty("title")).toBe(true)
+            expect(body.hasOwnProperty("article_id")).toBe(true)
+            expect(body.hasOwnProperty("body")).toBe(true)
+            expect(body.hasOwnProperty("topic")).toBe(true)
+            expect(body.hasOwnProperty("votes")).toBe(true)
+            expect(body.hasOwnProperty("comment_count")).toBe(true)
         })
-        test("STATUS:200 returns the expected values when given 1 as the parameter", () => {
+        test("STATUS:200 returns the expected values when given 1 as the parameter", async () => {
             const expectedOutput = {
                 article_id: 1,
                 title: 'Living in the shadow of a great man',
@@ -40,39 +32,32 @@ describe("/api/articles/:article_id", () => {
                 body: 'I find this existence challenging',
                 created_at: "2020-07-09T20:11:00.000Z",
                 votes: 100
-
             }
             
-            return request(app)
-            .get('/api/articles/1')
-            .then(({body}) => {
-                expect(body.article_id).toEqual(expectedOutput.article_id)
-                expect(body.title).toEqual(expectedOutput.title)
-                expect(body.topic).toEqual(expectedOutput.topic)
-                expect(body.author).toEqual(expectedOutput.author)
-                expect(body.body).toEqual(expectedOutput.body)
-                expect(body.created_at).toEqual(expectedOutput.created_at)
-                expect(body.votes).toEqual(expectedOutput.votes)
-            })
+            const {body} = await request(app).get('/api/articles/1')
+            expect(body.article_id).toEqual(expectedOutput.article_id)
+            expect(body.title).toEqual(expectedOutput.title)
+            expect(body.topic).toEqual(expectedOutput.topic)
+            expect(body.author).toEqual(expectedOutput.author)
+            expect(body.body).toEqual(expectedOutput.body)
+            expect(body.created_at).toEqual(expectedOutput.created_at)
+            expect(body.votes).toEqual(expectedOutput.votes)
         })
         test("STATUS:200 return the correct comment count", async () => {
             const {body} = await request(app).get('/api/articles/1')
             expect(body.comment_count).toBe("11")
         })
-
-        test("STATUS:404 when passed an articleID that does not exist in DB", () => {
-            return request(app)
-            .get('/api/articles/100')
-            .expect(404)
+        test("STATUS:404 when passed an articleID that does not exist in DB", async () => {
+            const output = await request(app).get('/api/articles/100')
+            expect(output.status).toBe(404)
         })
-        test("STATUS:400 when passed an articleID of the wrong data type", () => {
-            return request(app)
-            .get('/api/articles/banana')
-            .expect(400)
+        test("STATUS:400 when passed an articleID of the wrong data type", async () => {
+            const output = await request(app).get('/api/articles/banana')
+            expect(output.status).toBe(400)
         })
     })
     describe("PATCH", () => {
-        test("STATUS: 200 - responds with the updated article, votes increased when passed positive value", () => {
+        test("STATUS: 200 - responds with the updated article, votes increased when passed positive value", async () => {
             const expectedOutput = {
                 article_id: 1,
                 title: 'Living in the shadow of a great man',
@@ -82,16 +67,12 @@ describe("/api/articles/:article_id", () => {
                 created_at: "2020-07-09T20:11:00.000Z",
                 votes: 101
             }
-            
-            return request(app)
-            .patch('/api/articles/1')
-            .send({inc_votes: 1})
-            .expect(200)
-            .then(({body}) => {
-                expect(body).toEqual(expectedOutput)
-            })
+    
+            const output = await request(app).patch('/api/articles/1').send({inc_votes: 1})
+            expect(output.status).toBe(200)
+            expect(output.body).toEqual(expectedOutput)
         })
-        test("STATUS: 200 - responds with the updated article, votes decreased when passed negative value", () => {
+        test("STATUS: 200 - responds with the updated article, votes decreased when passed negative value", async () => {
             const expectedOutput = {
                 article_id: 1,
                 title: 'Living in the shadow of a great man',
@@ -101,40 +82,24 @@ describe("/api/articles/:article_id", () => {
                 created_at: "2020-07-09T20:11:00.000Z",
                 votes: 99
             }            
-            return request(app)
-            .patch('/api/articles/1')
-            .send({inc_votes: -2})
-            .expect(200)
-            .then(({body}) => {
-                expect(body).toEqual(expectedOutput)
-            })
+            const output = await request(app).patch('/api/articles/1').send({inc_votes: -2})
+            expect(output.status).toBe(200)
+            expect(output.body).toEqual(expectedOutput)
         })
-        test("status: 400 - when not passed a inc_votes body", () => {
-            return request(app)
-            .patch('/api/articles/1')
-            .send()
-            .expect(400)
-            .then(({body}) => {
-                expect(body.msg).toEqual("invalid data type")
-            })
+        test("status: 400 - when not passed a inc_votes body", async () => {
+            const output = await request(app).patch('/api/articles/1').send()
+            expect(output.status).toBe(400)
+            expect(output.body.msg).toEqual("invalid data type")
         })
-        test("status: 400 - when passed an invalid inc_votes charactor", () => {
-            return request(app)
-            .patch('/api/articles/1')
-            .send({inc_votes: "cat"})
-            .expect(400)
-            .then(({body}) => {
-                expect(body.msg).toEqual("invalid data type")
-            })
+        test("status: 400 - when passed an invalid inc_votes charactor", async () => {
+            const output = await request(app).patch('/api/articles/1').send({inc_votes: "cat"})
+            expect(output.status).toBe(400)
+            expect(output.body.msg).toEqual("invalid data type")
         })
-        test("status: 404 - when passed an article_id that does not exist", () => {
-            return request(app)
-            .patch('/api/articles/100')
-            .send({inc_votes: 1})
-            .expect(404)
-            .then(({body}) => {
-                expect(body.msg).toEqual("Requested ID not found")
-            })
+        test("status: 404 - when passed an article_id that does not exist", async () => {
+            const output = await request(app).patch('/api/articles/100').send({inc_votes: 1})
+            expect(output.status).toBe(404)
+            expect(output.body.msg).toEqual("Requested ID not found")
         })
     })
 })
