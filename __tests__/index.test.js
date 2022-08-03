c39b5c3b6a54f920adca2ced820ab7b7a6581cdb
lewispricey <1995ldp@gmail.com>
2022-08-03 12:22:10 +0100

    another test commit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,9 @@
-const {articleTest} = require("../indTests/articles.exam")
+const {articleTest} = require("../endpoint-tests/articles")
 const seedDB = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const db = require("../db/connection");
-const { topicsTests } = require("../indTests/topics.exam");
-const { userTests } = require("../indTests/users.exam");
+const { topicsTests } = require("../endpoint-tests/topics");
+const { userTests } = require("../endpoint-tests/users");
 
 beforeAll(() => seedDB(data))
 afterAll(() => db.end())
