9f76cbd1c30560ec4ba2ff231c4caefe3109c6ad
lewispricey <1995ldp@gmail.com>
2022-08-04 20:56:20 +0100

    adds production db script and listener

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
@@ -5,8 +6,21 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+const config = 
+  ENV === 'production'
+    ?{
+      connectionString: process.env.DATABASE_URL,
+      ssl:{
+        rejectUnauthorized: false,
+      },
+    }
+    :{}
+
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE URL not set');
+} else{
+  console.log("connected to " + ENV) //to remove after setup
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
