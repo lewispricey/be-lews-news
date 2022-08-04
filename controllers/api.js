90783a4ac4f2ff5393f396eff20b258145108fd6
lewispricey <1995ldp@gmail.com>
2022-08-04 16:57:06 +0100

    changes the /api endpoint to use fs to read the JSON file rarther than just requiring this

--- controllers/api.js
+++ controllers/api.js
@@ -1,2 +1,7 @@
-const endpoints = require("../endpoints.json")
-exports.getApi = (req, res, next) => res.status(200).send({endpoints})
+const { fetchEndpoints } = require("../models/api")
+
+exports.getApi = (req, res, next) => {
+    fetchEndpoints()
+    .then((endpointData) => res.status(200).send({endpoints: endpointData}))
+    .catch((err) => next(err))
+}
